53bac239d16bd17e9ab3ff659fe5af898e291d8e
fercho <fer.rom.mu@gmail.com>
2023-11-20 11:20:16 -0300

    Se agregan tests en notificaciones

--- src/test/kotlin/model/notification/TopicNotificationTest.kt
+++ src/test/kotlin/model/notification/TopicNotificationTest.kt
@@ -1,4 +1,56 @@
 package model.notification
 
+import model.AlertSystem
+import model.alert.Alert
+import model.alert.AlertPriority
+import model.topic.Receiver
+import model.topic.Topic
+import model.user.User
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.Duration
+import java.time.LocalDateTime
+
 class TopicNotificationTest {
+
+    lateinit var topic: Topic
+    lateinit var notification: TopicNotification
+    lateinit var oldDate: LocalDateTime
+    lateinit var futureDate: LocalDateTime
+
+    @BeforeEach
+    fun setup(){
+        topic = Topic("TemaA")
+        oldDate = LocalDateTime.now().minus(Duration.ofDays(10))
+        futureDate = LocalDateTime.now().plus(Duration.ofDays(10))
+    }
+
+    @Test
+    fun `La notificacion de un tema expira si su alerta tiene fecha de expiracion antigua`(){
+        notification = TopicNotification(
+            Alert(1, topic, AlertPriority.INFORMATIVA, oldDate),
+            Receiver.SINGLE_USER)
+
+        assertTrue(notification.isExpired())
+    }
+
+    @Test
+    fun `La notificacion de un tema no expira si su alerta tiene fecha de expiracion futura`(){
+        notification = TopicNotification(
+            Alert(1, topic, AlertPriority.INFORMATIVA, futureDate),
+            Receiver.SINGLE_USER)
+
+        assertFalse(notification.isExpired())
+    }
+
+    @Test
+    fun `La notificacion de un tema no expira si su alerta no tiene fecha de expiracion`(){
+        notification = TopicNotification(
+            Alert(1, topic, AlertPriority.INFORMATIVA),
+            Receiver.SINGLE_USER)
+
+        assertFalse(notification.isExpired())
+    }
 }
--- src/test/kotlin/model/notification/UserNotificationTest.kt
+++ src/test/kotlin/model/notification/UserNotificationTest.kt
@@ -1,4 +1,69 @@
 package model.notification
 
+import model.alert.Alert
+import model.alert.AlertPriority
+import model.topic.Receiver
+import model.topic.Topic
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.Duration
+import java.time.LocalDateTime
+
 class UserNotificationTest {
+
+    lateinit var topic: Topic
+    lateinit var notification: UserNotification
+    lateinit var oldDate: LocalDateTime
+    lateinit var futureDate: LocalDateTime
+
+    @BeforeEach
+    fun setup(){
+        topic = Topic("TemaA")
+        oldDate = LocalDateTime.now().minus(Duration.ofDays(10))
+        futureDate = LocalDateTime.now().plus(Duration.ofDays(10))
+    }
+
+    @Test
+    fun `La notificacion de un tema expira si su alerta tiene fecha de expiracion antigua`(){
+        notification = UserNotification(
+            Alert(1, topic, AlertPriority.INFORMATIVA, oldDate))
+
+        assertTrue(notification.isExpired())
+    }
+
+    @Test
+    fun `La notificacion de un tema no expira si su alerta tiene fecha de expiracion futura`(){
+        notification =  UserNotification(
+            Alert(1, topic, AlertPriority.INFORMATIVA, futureDate))
+
+        assertFalse(notification.isExpired())
+    }
+
+    @Test
+    fun `La notificacion de un tema no expira si su alerta no tiene fecha de expiracion`(){
+        notification =  UserNotification(
+            Alert(1, topic, AlertPriority.INFORMATIVA))
+
+        assertFalse(notification.isExpired())
+    }
+
+    @Test
+    fun `Por defecto la notificacion no esta leida`(){
+        notification =  UserNotification(
+            Alert(1, topic, AlertPriority.INFORMATIVA))
+
+        assertFalse(notification.isReaden)
+    }
+
+    @Test
+    fun `Al leer una notificacion figura como leida`(){
+        notification =  UserNotification(
+            Alert(1, topic, AlertPriority.INFORMATIVA))
+
+        notification.read()
+
+        assertTrue(notification.isReaden)
+    }
 }
